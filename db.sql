dd3006b5523457b8ee1b4c0f2834d4c51162b0d6
Julie Wright <jwright348@gatech.edu>
2020-10-31 16:23:33 -0700

    added some tables and data

--- db.sql
+++ db.sql
@@ -10,3 +10,24 @@
 --
 -- connect to database:
 -- \c database_name
+
+-- list all tables \d
+
+CREATE TABLE products (
+  id INT,
+  name VARCHAR(50),
+  price INT,
+  on_sale boolean
+);
+
+DROP TABLE restaurants;
+CREATE TABLE restaurants (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+);
+
+INSERT INTO restaurants (name, location, price_range) values ('mcdonalds', 'new yorks', 3);
+INSERT INTO restaurants (name, location, price_range) values ('pizza hut', 'vegas', 2);
+INSERT INTO restaurants (name, location, price_range) values ('wendys', 'seattle', 2);
